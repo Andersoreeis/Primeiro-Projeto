952af56bec68679353131110029706a0efcc51c8
Andersoreeis <91102169+Andersoreeis@users.noreply.github.com>
2021-12-10 22:20:50 -0300

    Add files via upload

---
+++ Página-1/script.js
@@ -0,0 +1,5 @@
+document.head.innerHTML="<style> body{ background-color: orangered;}</style>";
+var imagens = document.getElementById("tomada");
+ imagens.setAttribute("src","./image/acessa.png")
+imagens.setAttribute("title","Luz acessa")
+imagens.style = "position: absolute; top:50%; left:50%;transform: translate(-50%,-50%);"
